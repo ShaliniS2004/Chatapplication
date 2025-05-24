78f17eaa15fa0c20e8d9a07a0c67b09609ce7392
ShaliniS2004 <shalinivenkat2004@gmail.com>
2025-05-24 17:32:34 +0530

    first commit

---
+++ validation.js
@@ -0,0 +1,90 @@
+function printError(elemId, hintMsg) {
+     document.getElementById(elemId).innerHTML = hintMsg;
+    
+}
+
+function validateSignUpInputCredentials() {
+     username = document.getElementById("name").value;
+    email = document.getElementById("email_address").value;
+     password = document.getElementById("password").value;
+     confirmpassword = document.getElementById("confirm_password").value;
+    var nameErr = emailErr = passwordErr = confirmPasswordErr = true;
+    
+    if (username == "") {
+        printError("nameErr", "please enter your Name ");
+    } else {    
+        var regex = /^[A-Za-z ]+$/;
+        if (regex.test(username) === false) {
+            printError("nameErr", "please enter a valid Name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+
+    if (email == "") {
+        printError("emailErr", "please enter your email address");
+    } else {
+        var regex = /^\S+@\S+\.\S+$/;
+        if (regex.test(email) === false) {
+            printError("emailErr", "please enter a valid Email address");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+    
+    // ... rest of your validation code ...
+
+    
+    if(password == ""){
+        printError("passwordErr", "please enter your password");
+    }else{
+        printError("passwordErr", "");
+            passwordErr = false;
+    }
+    if(confirmpassword == ""){
+        printError("confirmPasswordErr", "please enter your password");
+    }else{
+        printError("confirmPasswordErr", "");
+        confirmPasswordErr = false;
+    }
+    if((nameErr || emailErr || passwordErr || confirmPasswordErr) == true) {
+        return false;
+    }else{
+        return true;
+    }
+}
+function validateLoginInputCredentials(){
+
+    email=document.getElementById("email_address").value;
+    password=document.getElementById("password").value;
+    var emailErr = passwordErr = true;
+    
+
+    if(email == ""){
+        printError("emailErr","Please Enter your email address");
+
+    } else{
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false){
+            printError("emailErr", "Please Enter your email address ");
+        } else {
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+
+    if(password == ""){
+        printError("passwordErr", "please enter your password");
+    }else{
+        printError("passwordErr", "");
+            passwordErr = false;
+    }
+    if(( emailErr || passwordErr ) == true) {
+        return false;
+    }else{
+        return true;
+    }
+    
+}
